b0c5af6928924213b629fc675cf0b2c44b03d8e3
Takahashi Saburo <takahashi@dhcpe4s045.a02.aist.go.jp>
2016-06-02 15:06:21 +0900

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# create directories
 mkdir -p ~/.vim/.swp
 mkdir -p ~/.vim/.undo
 mkdir -p ~/.vim/.backup
-
 mkdir -p ~/.vim/bundle
-git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
+# get neobundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+# get & set .vimrc
+git clone https://github.com/takahasi/vim
+ln -s vim/mac/.vimrc ~/.vimrc
